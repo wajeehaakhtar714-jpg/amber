0bc287b1d26e45e68482d6433f5362a8407b4e7c
wajeeha <wajeehaakhtar714@gmail.com>
2025-09-04 16:20:24 +0100

    project4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
-
+import Image from 'next/image'
 const page = () => {
   return (
     <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/img.jpg" alt="" width="384" height="512"/>
+  <Image
+      src="/profile.png"
+      alt="Picture of the author"
+      width={500}
+      height={500}
+    />
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
+        &quot;making my next.js project,&quot;
       </p>
     </blockquote>
     <figcaption className="font-medium">
